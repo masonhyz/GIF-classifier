1c50bbcee7c877a453bb09db36773bbbc4b66183
Johnny Meng <jmyw139@gmail.com>
2024-04-14 20:46:55 -0400

    Changed parameters

--- model_2_1dconv.py
+++ model_2_1dconv.py
@@ -9,14 +9,15 @@ class Conv2_1d(nn.Module):
     def __init__(self,
                  in_channels: int,
                  out_channels: int,
-                 kernel_size: Tuple[int, int],
+                 kernel_size: Tuple[int],
                  hidden_size: Optional[int]=None,
-                 stride: Tuple[int, int]=(1, 1),
-                 padding: Tuple[int, int]=(0, 0),) -> None:
+                 stride: Tuple[int]=(1, 1, 1, 1),
+                 padding: Tuple[int]=(0, 0, 0, 0)) -> None:
         """
-        Note: kernel_sizes, strides, and paddings all take a tuple of 2 integers,
-        where the first element is the value for the 2d conv layer, and the
-        second element is for the 1d conv layer.
+        Note: kernel_sizes, strides, and paddings all take a tuple of 4 integers,
+        where the first element is the value for the 2d conv layer, the
+        second element for the 1d conv layer, the third for the 2d max
+        pooling, and the fourth for the 1d max pooling.
         """
         super(Conv2_1d, self).__init__()
 
@@ -39,6 +40,12 @@ class Conv2_1d(nn.Module):
                                 kernel_size[1],
                                 stride[1],
                                 padding[1])
+        self.pool2d = nn.MaxPool2d(kernel_size[2],
+                                   stride[2],
+                                   padding[2])
+        self.pool1d = nn.MaxPool1d(kernel_size[3],
+                                   stride[3],
+                                   padding[3])
         
     def forward(self, x):
         # shape [b, t, c, h, w]
@@ -48,13 +55,19 @@ class Conv2_1d(nn.Module):
         x = self.conv2d(x)
         x = self.relu(x)
 
+        # 2d pooling
+        x = self.pool2d(x)
+
         # 1d conv layer
         c, h, w = x.size(1), x.size(2), x.size(3)
         x = x.reshape(b, t, c, h, w)
         x = x.permute(0, 3, 4, 2, 1) # shape [b, h, w, c, t]
         x = x.reshape(b * h * w, c, t)
         x = self.conv1d(x)
-        output = self.relu(x)
+        x = self.relu(x)
+
+        # 1d pooling
+        output = self.pool1d(x)
 
         # Output
         c, t = output.size(1), output.size(2)
@@ -74,22 +87,19 @@ class GIFClassifier(nn.Module):
         # general shape [b, t, c, h, w]
         # input shape [b, 50, 3, 500, 500]
 
-        # shape [b, 24, 32, 164, 164]
-        self.conv1 = Conv2_1d(3, 32, kernel_size=(9, 3), stride=(3, 2))
-        # shape [b, 12, 64, 80, 80]
-        self.conv2 = Conv2_1d(32, 64, kernel_size=(5, 2), stride=(2, 2))
-        # shape [b, 6, 128, 38, 38]
-        self.conv3 = Conv2_1d(64, 128, kernel_size=(5, 2), stride=(2, 2))
-        # shape [b, 2, 128, 18, 18]
-        self.conv4 = Conv2_1d(128, 128, kernel_size=(3, 3), stride=(2, 2))
-        # shape [b, 1, 64, 8, 8]
-        self.conv5 = Conv2_1d(128, 64, kernel_size=(3, 2), stride=(2, 1))
+        # shape [b, 12, 32, 125, 125]
+        self.conv1 = Conv2_1d(3, 32, kernel_size=(11, 3, 4, 4), stride=(1, 1, 4, 4), padding=(10, 2, 0, 0))
+        # shape [b, 6, 64, 31, 31]
+        self.conv2 = Conv2_1d(32, 64, kernel_size=(5, 3, 4, 2), stride=(1, 1, 4, 2), padding=(4, 2, 0, 0))
+        # shape [b, 3, 128, 15, 15]
+        self.conv3 = Conv2_1d(64, 128, kernel_size=(3, 3, 2, 2), stride=(1, 1, 2, 2), padding=(2, 2, 0, 0))
+        # shape [b, 3, 64, 7, 7]
+        self.conv4 = Conv2_1d(128, 64, kernel_size=(3, 1, 2, 1), stride=(1, 1, 2, 2), padding=(2, 0, 0, 0))
 
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(64 * 8 * 8, 4096)
+        self.fc1 = nn.Linear(9408, 4096)
         self.fc2 = nn.Linear(4096, 2048)
-        self.fc3 = nn.Linear(2048, 1024)
-        self.out = nn.Linear(1024, num_classes)
+        self.out = nn.Linear(2048, num_classes)
 
     def forward(self, x):
         # 2+1d conv layers
@@ -97,15 +107,12 @@ class GIFClassifier(nn.Module):
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-        x = self.conv5(x)
 
         # fc layers
         x = x.flatten(1)
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
         x = self.relu(x)
         logits = self.out(x)
 
